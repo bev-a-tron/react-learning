84cf242cd46198d5681ae2b8221aaf97df24e91e
Beverly Lau <beverly.a.lau@gmail.com>
2015-08-01 14:17:03 -0700

    Combining components into parent component

--- fruit_script.js
+++ fruit_script.js
@@ -1,14 +1,33 @@
+var FruitComponent = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <BananaComponent />
+                <AppleComponent />
+            </div>
+        )
+    }
+});
+
 var BananaComponent = React.createClass({
     render: function() {
         return (
             <img src="static/bananas.png"></img>
-        );
+        )
+    }
+});
+
+var AppleComponent = React.createClass({
+    render: function() {
+        return (
+            <img src="static/apple.png"></img>
+        )
     }
 });
 
 React.render(
-    <BananaComponent />,
-    document.getElementById('bananas')
+    <FruitComponent />,
+    document.getElementById('fruit')
 );
 
 
